e4a7a356866a678910a640d4a5ad4f8f678b054d
padre_paulas <pounowyou@gmail.com>
2026-01-27 20:25:24 +0200

    Print Title on the screen

---
+++ src/Quicksort.jsx
@@ -0,0 +1,7 @@
+const Quicksort = () => {
+  return <div className="text-sky-400 bg-sky-200 flex m-auto items-center text-center justify-center h-screen w-screen">
+    <h1 className="flex items-center text-center justify-center">Quicksort</h1>
+  </div>
+}
+
+export default Quicksort;
