3ba513f483ba0fb2276bd4d1a4e0fa139d103964
padre_paulas <pounowyou@gmail.com>
2026-02-01 16:56:58 +0200

    add refs to each array bar div

--- src/Quicksort/QuicksortBars.jsx
+++ src/Quicksort/QuicksortBars.jsx
@@ -1,9 +1,9 @@
-const QuicksortBars = ({ array = [] }) => {
+const QuicksortBars = ({ array = [], barRefs }) => {
   const width = 100 / array.length;
   console.log(array + "inside the quicksortBars")
-  return <div className="flex items-end place-self-center w-[98%] h-full">
+  return <div  className="flex items-end place-self-center w-[98%] h-full">
     {array.map((element, i) => {
-      return <div key={i}
+      return <div key={i} ref={(element) => barRefs.current[i] = element}
       className={`
         bg-amber-glow flex rounded-sm border border-midnight-violet/30`} style={{ 
         height: `${element}%`, width: `${width}%` 
