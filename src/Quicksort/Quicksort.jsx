d81c8d45b85093d4ed3ffd39b9a97e7ee0dba08b
padre_paulas <pounowyou@gmail.com>
2026-02-01 14:14:33 +0200

    update the quicksort bars field style

--- src/Quicksort/Quicksort.jsx
+++ src/Quicksort/Quicksort.jsx
@@ -14,8 +14,8 @@ const Quicksort = () => {
     flex flex-col items-center gap-4 
     ">
     <h1 className="flex items-center text-center justify-center text-4xl mt-4">Quicksort</h1>
-    <div className={`bg-amber-glow/20 w-[70vw] h-[70vh] rounded-xl 
-    ${/* inset-shadow-[0_0_8px_var(--color-midnight-violet)] */' '}`}>
+    <div className={` w-[70vw] h-[70vh] rounded-xl shadow-[0_0_8px_var(--color-midnight-violet)]
+    ${/* inset-shadow-[0_0_8px_var(--color-midnight-violet)] */' '} `}>
       <QuicksortBars array={array} />
     </div>
 
@@ -25,7 +25,7 @@ const Quicksort = () => {
     text-xl transition-all ease-in-out 
     hover:bg-muted-teal/90 hover:border-muted-teal
     active:text-xs
-    ">Start</button>
+    ">START</button>
       
     </div>
     <div className="grid justify-center text-center m-2
