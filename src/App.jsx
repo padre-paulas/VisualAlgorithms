da1fe11ad757aec4c6acc755d74ab39d52649ddd
padre_paulas <pounowyou@gmail.com>
2026-02-03 16:34:10 +0200

    remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-//import { useState } from 'react'
 import './App.css'
 import Quicksort from './sorting-algorithms/Quicksort/Quicksort'
 import Bubblesort from './sorting-algorithms/Bubblesort/Bubblesort'
 
 function App() {
-  //const [count, setCount] = useState(0)
-
   return (
     <>
       {/* <Quicksort/> */}
