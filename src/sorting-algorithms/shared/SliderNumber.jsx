58f82cbd2621cd7039fb1a83312e1b2c9712c70b
padre_paulas <pounowyou@gmail.com>
2026-02-06 13:27:29 +0200

    create setColor utility function to reset array bars color

--- src/sorting-algorithms/shared/SliderNumber.jsx
+++ src/sorting-algorithms/shared/SliderNumber.jsx
@@ -1,6 +1,6 @@
 import generateArray from "./generate-array";
 
-const SliderNumber = ({ value, onChange, setArray }) => {
+const SliderNumber = ({ value, onChange, setArray, barRefs }) => {
   return <input className="h-1 w-35 bg-transparent" 
     min="1" max="100" value={value} type="range"
     style={{ accentColor: 'var(--color-midnight-violet)' }}
@@ -8,7 +8,16 @@ const SliderNumber = ({ value, onChange, setArray }) => {
       onChange(e.target.value);
       setArray(generateArray(e.target.value));
     }}
+    onMouseDown={(e) => {
+      setColor(barRefs, e.target.value);
+    }}
     ></input>
+}
+
+const setColor = (barRefs, value) =>  {
+  for (let i = 0; i < value; i++) {
+    barRefs.current[i].style.background = 'var(--color-amber-glow)';
+  }
 }
 
 export default SliderNumber;
