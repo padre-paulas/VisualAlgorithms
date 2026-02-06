ac07e3a03a4e3fb2baa2cac82db99488a903c2c6
padre_paulas <pounowyou@gmail.com>
2026-02-06 14:34:36 +0200

    change SliderTime max value to 500 ms

--- src/sorting-algorithms/shared/SliderTime.jsx
+++ src/sorting-algorithms/shared/SliderTime.jsx
@@ -1,6 +1,6 @@
 const SliderTime = ({ value, onChange }) => {
   return <input className="h-1 w-35 bg-transparent" 
-    min="1" max="1000" value={value} type="range"
+    min="1" max="500" value={value} type="range"
     style={{ accentColor: 'var(--color-midnight-violet)' }}
     onChange={(e) => {
       onChange(e.target.value);
