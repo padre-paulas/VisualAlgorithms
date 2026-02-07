5a59106e10cc8b46355bcaefc3d79e03acc94dfa
padre_paulas <pounowyou@gmail.com>
2026-02-07 16:56:55 +0200

    display Simple Search page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Quicksort from './sorting-algorithms/Quicksort/Quicksort'
 import Bubblesort from './sorting-algorithms/Bubblesort/Bubblesort'
 import BinarySearch from './search-algorithms/BinarySearch/BinarySearch'
+import SimpleSearch from './search-algorithms/SimpleSearch/SimpleSearch'
 
 function App() {
   return (
     <>
       {/* <Quicksort/> */}
       {/* <Bubblesort /> */}
-      <BinarySearch />
+      {/* <BinarySearch /> */}
+      <SimpleSearch />
     </>
   )
 }
