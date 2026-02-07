181f1870d9c8d91860f726526d9e661fe45224b3
padre_paulas <pounowyou@gmail.com>
2026-02-07 16:55:00 +0200

    create simple search function

--- src/search-algorithms/SimpleSearch/simple-search-func.js
+++ src/search-algorithms/SimpleSearch/simple-search-func.js
@@ -1,5 +1,17 @@
-const simpleSearchFunc = () => {
-  return;
+import setColor from "../../shared-global/set-array-bars-color";
+import sleep from "../../shared-global/sleep";
+
+const simpleSearchFunc = async (array, target, barRefs, timePerOperation) => {
+  setColor(barRefs, array.length, 'var(--color-amber-glow)')
+  for (let i = 0; i < array.length; i++) {
+    barRefs.current[i].style.background = 'var(--color-vibrant-coral)';
+    await sleep(timePerOperation);
+    if (i === target) {
+      barRefs.current[i].style.background = 'var(--color-midnight-violet)';
+      return i;
+    }
+    barRefs.current[i].style.background = 'var(--color-amber-glow)';
+  }
 }
 
 export default simpleSearchFunc;
