dc988bcd9529083c6b114cd467d194d6845ebb88
padre_paulas <pounowyou@gmail.com>
2026-02-07 15:29:06 +0200

    floor all array elements in generate sorted array function

--- src/shared-global/generate-array-sorted.js
+++ src/shared-global/generate-array-sorted.js
@@ -1,6 +1,6 @@
 const generateArraySorted = (numberOfElements) => {
   let array = Array.from({ length: numberOfElements  },
-  (el, i) => (i + 1) * (99 / numberOfElements)
+  (el, i) => Math.floor((i + 1) * (99 / numberOfElements))
   );
   return array;
 }
