1a49801f9d2237170be6e0a77bad7f5d30170a7f
padre_paulas <pounowyou@gmail.com>
2026-02-07 15:28:34 +0200

    create start button for search algorithms

--- src/search-algorithms/shared/ButtonStart.jsx
+++ src/search-algorithms/shared/ButtonStart.jsx
@@ -1,7 +1,14 @@
-const ButtonStart = () => {
-  return <div>
-
-  </div>
+const ButtonStart = ({ array, barRefs, timePerOperation, searchFunc }) => {
+  const target = 19;
+  return <button onClick={() => {
+    searchFunc(array, target, barRefs, timePerOperation);
+  }}
+    className="bg-muted-teal rounded-xl w-30 h-12 
+    shadow-[0_0_3px_var(--color-teal-700)] border-2 border-teal-700
+    text-xl transition-all ease-in-out 
+    hover:bg-muted-teal/90 hover:border-muted-teal
+    active:text-xs
+    ">START</button>
 }
 
 export default ButtonStart;
